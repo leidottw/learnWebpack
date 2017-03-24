84e2a6b7591eb078c9cbfcef214f00cb9573d2d9
leidottw <leidottw@gmail.com>
2017-03-24 16:40:21 +0800

    try prosemirror

--- src/components/ProseMirror.js
+++ src/components/ProseMirror.js
@@ -3,9 +3,10 @@ import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 // import { DOMParser } from 'prosemirror-model';
 import { schema } from 'prosemirror-schema-basic';
-// import { exampleSetup } from 'prosemirror-example-setup';
+import { exampleSetup, buildMenuItems } from 'prosemirror-example-setup';
 import History from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
+import { menuBar, MenuItem } from 'prosemirror-menu';
 
 class ProseMirror extends React.Component {
     constructor(props) {
@@ -13,15 +14,29 @@ class ProseMirror extends React.Component {
     }
 
     componentDidMount() {
+        let menu = buildMenuItems(schema);
+        menu.insertMenu.content = [new MenuItem({
+            title: 'xxxTitle',
+            label: 'xxxLabel',
+            select(state) {
+                console.log('select');
+                console.log(state);
+            },
+            run(state, dispatch) {
+                console.log('run');
+            }
+        })].concat(menu.insertMenu.content);
+console.log(exampleSetup({schema, menuContent: menu.fullMenu}))
         this.editorState = EditorState.create({
             schema,
-            plugins: [
-                History.history(),
-                keymap({
-                    'Mod-z': History.undo,
-                    'Mod-y': History.redo
-                })
-            ]
+            plugins: exampleSetup({schema, menuContent: menu.fullMenu})
+            // plugins: [
+            //     History.history(),
+            //     keymap({
+            //         'Mod-z': History.undo,
+            //         'Mod-y': History.redo
+            //     })
+            // ]
         });
 
         this.editor = new EditorView(this.refs.editor, {
