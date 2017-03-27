883a9b166052ebf4eb6fb07d85bfdbfd6f3cfc69
leidottw <leidottw@gmail.com>
2017-03-27 19:04:27 +0800

    try prosemirror

--- src/components/ProseMirror.js
+++ src/components/ProseMirror.js
@@ -3,10 +3,11 @@ import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 // import { DOMParser } from 'prosemirror-model';
 import { schema } from 'prosemirror-schema-basic';
-import { exampleSetup, buildMenuItems } from 'prosemirror-example-setup';
-import History from 'prosemirror-history';
+import { exampleSetup, buildMenuItems, buildKeymap } from 'prosemirror-example-setup';
+import { history } from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
 import { menuBar, MenuItem } from 'prosemirror-menu';
+import { toggleMark } from 'prosemirror-commands';
 
 class ProseMirror extends React.Component {
     constructor(props) {
@@ -14,29 +15,35 @@ class ProseMirror extends React.Component {
     }
 
     componentDidMount() {
-        let menu = buildMenuItems(schema);
-        menu.insertMenu.content = [new MenuItem({
-            title: 'xxxTitle',
-            label: 'xxxLabel',
-            select(state) {
-                console.log('select');
-                console.log(state);
-            },
-            run(state, dispatch) {
-                console.log('run');
-            }
-        })].concat(menu.insertMenu.content);
-console.log(exampleSetup({schema, menuContent: menu.fullMenu}))
+        console.log(exampleSetup({schema}))
+        let r = buildMenuItems(schema);
+        console.log(r);
+
         this.editorState = EditorState.create({
             schema,
-            plugins: exampleSetup({schema, menuContent: menu.fullMenu})
-            // plugins: [
-            //     History.history(),
-            //     keymap({
-            //         'Mod-z': History.undo,
-            //         'Mod-y': History.redo
-            //     })
-            // ]
+            // plugins: exampleSetup({schema})
+            plugins: [
+                history(),
+                menuBar({
+                    floating: true,
+                    // content: [[r.toggleStrong]]
+                    content: [[new MenuItem({
+                        title: 'testTitle',
+                        label: 'testLabel',
+                        select(state) {
+                            console.log('state');
+                            return state;
+                        },
+                        run(state, dispatch) {
+                            console.log('run');
+                        }
+                    })]]
+                }),
+                keymap({
+                    'Mod-z': History.undo,
+                    'Mod-y': History.redo
+                })
+            ]
         });
 
         this.editor = new EditorView(this.refs.editor, {
