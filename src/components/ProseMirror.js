a17481fc9b89e899e4c76742f4380626cbc4bd83
leidottw <leidottw@gmail.com>
2017-03-28 19:07:28 +0800

    study prosemirror

--- src/components/ProseMirror.js
+++ src/components/ProseMirror.js
@@ -4,10 +4,10 @@ import { EditorView } from 'prosemirror-view';
 // import { DOMParser } from 'prosemirror-model';
 import { schema } from 'prosemirror-schema-basic';
 import { exampleSetup, buildMenuItems, buildKeymap } from 'prosemirror-example-setup';
-import { history } from 'prosemirror-history';
+import { history, undo, redo } from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
-import { menuBar, MenuItem } from 'prosemirror-menu';
-import { toggleMark } from 'prosemirror-commands';
+import { menuBar, MenuItem, Dropdown, DropdownSubmenu, renderGrouped, icons, joinUpItem, liftItem, selectParentNodeItem, undoItem, redoItem, wrapItem, blockTypeItem } from 'prosemirror-menu'; // menuBar提供浮動置頂,
+import { baseKeymap, toggleMark } from 'prosemirror-commands';
 
 class ProseMirror extends React.Component {
     constructor(props) {
@@ -25,23 +25,80 @@ class ProseMirror extends React.Component {
             plugins: [
                 history(),
                 menuBar({
-                    floating: true,
+                    floating: true, // toolbar 浮動置頂
                     // content: [[r.toggleStrong]]
-                    content: [[new MenuItem({
-                        title: 'testTitle',
-                        label: 'testLabel',
-                        select(state) {
-                            console.log('state');
-                            return state;
-                        },
-                        run(state, dispatch) {
-                            console.log('run');
-                        }
-                    })]]
+                    content: [[
+                        new MenuItem({
+                            select(state) {
+                                // 判斷按鈕是否可使用
+                                return true;
+                            },
+                            onDeselected: "disable", // 決定按鈕不可使用時的處理方式, 預設為隱藏按鈕, 設為"disable"時則反灰該按鈕
+                            active(state) {
+                                // check active狀態
+                                // 若這顆按鈕是on的狀態回傳true
+                                // off則回傳false
+                                // 若是active狀態
+                                // prosemirror會在按鈕上加上class "ProseMirror-menu-active"
+                                return false;
+                            },
+                            // render,
+                            icon: { // 詳情請study prosemirror-menu/src/icon.js
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.className = 'images grid';
+                                    return node;
+                                })(),
+                            },
+                            label: '', //
+                            title: '按鈕',
+                            class: '', // 對icon加上class
+                            css: '', // 對icon加上inline css
+                            execEvent: '', // 觸發事件 default: mousedown
+                            run(state, dispatch) {
+                                console.log(state)
+                                console.log('run');
+                            }
+                        }),
+                        joinUpItem,
+                        liftItem,
+                        selectParentNodeItem,
+                        undoItem,
+                        redoItem,
+                        new Dropdown([
+                            new MenuItem({
+                                label: '選項1',
+                                title: '選項1',
+                                select() {
+                                    return false;
+                                },
+                                onDeselected: "disable",
+                                run() {
+                                    console.log('run 1');
+                                }
+                            }),
+                            new MenuItem({
+                                label: '選項2',
+                                title: '選項2',
+                                select() {
+                                    return true;
+                                },
+                                run() {
+                                    console.log('run 2');
+                                }
+                            })
+                        ], {
+                            label: '下拉選單',
+                            title: '下拉選單',
+                            class: '',
+                            css: ''
+                        })
+                    ]]
                 }),
+                keymap(baseKeymap),
                 keymap({
-                    'Mod-z': History.undo,
-                    'Mod-y': History.redo
+                    'Mod-z': undo,
+                    'Mod-y': redo
                 })
             ]
         });
