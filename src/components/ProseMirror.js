c36504facb170eacf055b848e28a453220031e60
leidottw <leidottw@gmail.com>
2017-03-29 18:39:09 +0800

    implement editor

--- src/components/ProseMirror.js
+++ src/components/ProseMirror.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
-// import { DOMParser } from 'prosemirror-model';
-import { schema } from 'prosemirror-schema-basic';
+import { Schema, DOMParser } from 'prosemirror-model';
+import { nodes, marks } from 'prosemirror-schema-basic';
 import { exampleSetup, buildMenuItems, buildKeymap } from 'prosemirror-example-setup';
 import { history, undo, redo } from 'prosemirror-history';
 import { keymap } from 'prosemirror-keymap';
@@ -15,9 +15,29 @@ class ProseMirror extends React.Component {
     }
 
     componentDidMount() {
-        console.log(exampleSetup({schema}))
-        let r = buildMenuItems(schema);
-        console.log(r);
+        let extMarks = Object.assign({}, marks, {
+            u: {
+                parseDOM: [{tag: 'u'}],
+                toDOM() {
+                    return ['u'];
+                }
+            },
+            del: {
+                parseDOM: [{tag: 'del'}],
+                toDOM() {
+                    return ['del'];
+                }
+            }
+        });
+
+        let schema = new Schema({
+            nodes,
+            marks: extMarks
+        });
+
+        // console.log(exampleSetup({schema}))
+        // let r = buildMenuItems(schema);
+        // console.log(r);
 
         this.editorState = EditorState.create({
             schema,
@@ -30,69 +50,326 @@ class ProseMirror extends React.Component {
                     content: [[
                         new MenuItem({
                             select(state) {
-                                // 判斷按鈕是否可使用
-                                return true;
+                                return toggleMark(schema.marks.strong)(state);
                             },
-                            onDeselected: "disable", // 決定按鈕不可使用時的處理方式, 預設為隱藏按鈕, 設為"disable"時則反灰該按鈕
                             active(state) {
-                                // check active狀態
-                                // 若這顆按鈕是on的狀態回傳true
-                                // off則回傳false
-                                // 若是active狀態
-                                // prosemirror會在按鈕上加上class "ProseMirror-menu-active"
-                                return false;
+                                let {from, $from, to, empty} = state.selection;
+                                if(empty) {
+                                    // 未框選時check游標是否在樣式的範圍內
+                                    return schema.marks.strong.isInSet(state.storedMarks || $from.marks())
+                                } else {
+                                    // 框選時check框選的部分是否包含有套用樣式的範圍
+                                    return state.doc.rangeHasMark(from, to, schema.marks.strong)
+                                }
                             },
-                            // render,
-                            icon: { // 詳情請study prosemirror-menu/src/icon.js
+                            icon: {
                                 dom: (() => {
                                     let node = document.createElement('div');
-                                    node.className = 'images grid';
+                                    node.innerText = '粗體';
                                     return node;
-                                })(),
+                                })()
                             },
-                            label: '', //
-                            title: '按鈕',
-                            class: '', // 對icon加上class
-                            css: '', // 對icon加上inline css
-                            execEvent: '', // 觸發事件 default: mousedown
-                            run(state, dispatch) {
-                                console.log(state)
-                                console.log('run');
+                            label: '',
+                            title: '粗體',
+                            run: toggleMark(schema.marks.strong)
+                        }),
+                        new MenuItem({
+                            select(state) {
+                                return toggleMark(schema.marks.em)(state);
+                            },
+                            active(state) {
+                                let {from, $from, to, empty} = state.selection;
+                                if(empty) {
+                                    // 未框選時check游標是否在樣式的範圍內
+                                    return schema.marks.em.isInSet(state.storedMarks || $from.marks())
+                                } else {
+                                    // 框選時check框選的部分是否包含有套用樣式的範圍
+                                    return state.doc.rangeHasMark(from, to, schema.marks.em)
+                                }
+                            },
+                            icon: {
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.innerText = '斜體';
+                                    return node;
+                                })()
+                            },
+                            label: '',
+                            title: '斜體',
+                            run: toggleMark(schema.marks.em)
+                        }),
+                        new MenuItem({
+                            select(state) {
+                                return toggleMark(schema.marks.u)(state);
+                            },
+                            active(state) {
+                                let {from, $from, to, empty} = state.selection;
+                                if(empty) {
+                                    // 未框選時check游標是否在樣式的範圍內
+                                    return schema.marks.u.isInSet(state.storedMarks || $from.marks())
+                                } else {
+                                    // 框選時check框選的部分是否包含有套用樣式的範圍
+                                    return state.doc.rangeHasMark(from, to, schema.marks.u)
+                                }
+                            },
+                            icon: {
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.innerText = '底線';
+                                    return node;
+                                })()
+                            },
+                            label: '',
+                            title: '底線',
+                            run: toggleMark(schema.marks.u)
+                        }),
+                        new MenuItem({
+                            select(state) {
+                                return toggleMark(schema.marks.del)(state);
+                            },
+                            active(state) {
+                                let {from, $from, to, empty} = state.selection;
+                                if(empty) {
+                                    // 未框選時check游標是否在樣式的範圍內
+                                    return schema.marks.del.isInSet(state.storedMarks || $from.marks())
+                                } else {
+                                    // 框選時check框選的部分是否包含有套用樣式的範圍
+                                    return state.doc.rangeHasMark(from, to, schema.marks.del)
+                                }
+                            },
+                            icon: {
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.innerText = '刪除線';
+                                    return node;
+                                })()
+                            },
+                            label: '',
+                            title: '刪除線',
+                            run: toggleMark(schema.marks.del)
+                        }),
+                        new MenuItem({
+                            select(state) {
+                                return toggleMark(schema.marks.link)(state);
+                            },
+                            active(state) {
+                                let {from, $from, to, empty} = state.selection;
+                                if(empty) {
+                                    // 未框選時check游標是否在樣式的範圍內
+                                    return schema.marks.link.isInSet(state.storedMarks || $from.marks())
+                                } else {
+                                    // 框選時check框選的部分是否包含有套用樣式的範圍
+                                    return state.doc.rangeHasMark(from, to, schema.marks.link)
+                                }
+                            },
+                            icon: {
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.innerText = '連結';
+                                    return node;
+                                })()
+                            },
+                            label: '',
+                            title: '連結',
+                            run(state, dispatch, view) {
+                                if (this.active(state)) {
+                                    toggleMark(schema.marks.link)(state, dispatch);
+                                    return true;
+                                }
+
+                                let title = prompt('輸入名稱:'),
+                                    href = prompt('輸入網址:');
+
+                                toggleMark(schema.marks.link, { title, href })(state, dispatch);
+                                view.focus();
                             }
                         }),
-                        joinUpItem,
-                        liftItem,
-                        selectParentNodeItem,
-                        undoItem,
-                        redoItem,
+                        new MenuItem({
+                            select(state) {
+                                let $from = state.selection.$from,
+                                    attrs = undefined;
+
+                                for (let d = $from.depth; d >= 0; d--) {
+                                    let index = $from.index(d);
+
+                                    if ($from.node(d).canReplaceWith(index, index, schema.nodes.image, attrs)) {
+                                        return true;
+                                    }
+                                }
+
+                                return false;
+                            },
+                            icon: {
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.innerText = '插入圖片';
+                                    return node;
+                                })()
+                            },
+                            label: '',
+                            title: '插入圖片',
+                            run(state, dispatch, view) {
+                                let {node, from, to} = state.selection,
+                                    attrs = schema.nodes.image && node && node.type == schema.nodes.image && node.attrs;
+
+                                let title = prompt('輸入名稱:'),
+                                    alt = prompt('替代名稱:'),
+                                    src = prompt('輸入網址:');
+
+                                dispatch(state.tr.replaceSelectionWith(schema.nodes.image.createAndFill({ title, alt, src })));
+                                view.focus();
+                            }
+                        }),
+                        new MenuItem({
+                            icon: {
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.innerText = '**插入檔案';
+                                    return node;
+                                })()
+                            },
+                            label: '',
+                            title: '**插入檔案',
+                            run(state, dispatch, view) {
+                                alert('待實作');
+                            }
+                        }),
+                        new MenuItem({
+                            select(state) {
+                                let $from = state.selection.$from,
+                                    attrs = undefined;
+
+                                for (let d = $from.depth; d >= 0; d--) {
+                                    let index = $from.index(d);
+
+                                    if ($from.node(d).canReplaceWith(index, index, schema.nodes.horizontal_rule, attrs)) {
+                                        return true;
+                                    }
+                                }
+
+                                return false;
+                            },
+                            icon: {
+                                dom: (() => {
+                                    let node = document.createElement('div');
+                                    node.innerText = '插入分隔線';
+                                    return node;
+                                })()
+                            },
+                            label: '',
+                            title: '插入分隔線',
+                            run(state, dispatch, view) {
+                                dispatch(state.tr.replaceSelectionWith(schema.nodes.horizontal_rule.create()));
+                                view.focus();
+                            }
+                        }),
                         new Dropdown([
                             new MenuItem({
-                                label: '選項1',
-                                title: '選項1',
-                                select() {
-                                    return false;
-                                },
-                                onDeselected: "disable",
-                                run() {
-                                    console.log('run 1');
-                                }
+                                label: '置左'
                             }),
                             new MenuItem({
-                                label: '選項2',
-                                title: '選項2',
-                                select() {
-                                    return true;
-                                },
-                                run() {
-                                    console.log('run 2');
-                                }
+                                label: '置中'
+                            }),
+                            new MenuItem({
+                                label: '置右'
                             })
                         ], {
-                            label: '下拉選單',
-                            title: '下拉選單',
-                            class: '',
-                            css: ''
+                            label: '**對齊',
+                            title: '**對齊'
+                        }),
+                        new Dropdown([
+                            new MenuItem({
+                                label: '程式碼'
+                            }),
+                            new MenuItem({
+                                label: '標題1'
+                            }),
+                            new MenuItem({
+                                label: '標題2'
+                            }),
+                            new MenuItem({
+                                label: '標題3'
+                            }),
+                            new MenuItem({
+                                label: '標題4'
+                            }),
+                            new MenuItem({
+                                label: '標題5'
+                            }),
+                            new MenuItem({
+                                label: '標題6'
+                            })
+                        ], {
+                            label: '**樣式',
+                            title: '**樣式'
                         })
+                        // MenuItem Template
+                        // new MenuItem({
+                        //     select(state) {
+                        //         // 判斷按鈕是否可使用
+                        //         return true;
+                        //     },
+                        //     onDeselected: "disable", // 決定按鈕不可使用時的處理方式, 預設為隱藏按鈕, 設為"disable"時則反灰該按鈕
+                        //     active(state) {
+                        //         // check active狀態
+                        //         // 若這顆按鈕是on的狀態回傳true
+                        //         // off則回傳false
+                        //         // 若是active狀態
+                        //         // prosemirror會在按鈕上加上class "ProseMirror-menu-active"
+                        //         return false;
+                        //     },
+                        //     // render,
+                        //     icon: { // 詳情請study prosemirror-menu/src/icon.js
+                        //         dom: (() => {
+                        //             let node = document.createElement('div');
+                        //             node.className = 'images grid';
+                        //             return node;
+                        //         })()
+                        //     },
+                        //     label: '',
+                        //     title: '按鈕',
+                        //     class: '', // 對icon加上class
+                        //     css: '', // 對icon加上inline css
+                        //     execEvent: '', // 觸發事件 default: mousedown
+                        //     run(state, dispatch) {
+                        //         console.log(state)
+                        //         console.log('run');
+                        //     }
+                        // }),
+                        // Dropdown Template
+                        // new Dropdown([
+                        //     new MenuItem({
+                        //         label: '選項1',
+                        //         title: '選項1',
+                        //         select() {
+                        //             return false;
+                        //         },
+                        //         onDeselected: "disable",
+                        //         run() {
+                        //             console.log('run 1');
+                        //         }
+                        //     }),
+                        //     new MenuItem({
+                        //         label: '選項2',
+                        //         title: '選項2',
+                        //         select() {
+                        //             return true;
+                        //         },
+                        //         run() {
+                        //             console.log('run 2');
+                        //         }
+                        //     })
+                        // ], {
+                        //     label: '下拉選單',
+                        //     title: '下拉選單',
+                        //     class: '',
+                        //     css: ''
+                        // }),
+                        // joinUpItem,
+                        // liftItem,
+                        // selectParentNodeItem,
+                        // undoItem,
+                        // redoItem
                     ]]
                 }),
                 keymap(baseKeymap),
--- webpack.config.js
+++ webpack.config.js
@@ -76,6 +76,7 @@ module.exports = {
         contentBase: path.join(__dirname, 'dev'),
         compress: true,
         historyApiFallback: true,
+        port: 8088,
         proxy: {
             '/api': 'http://172.17.20.49:8080/video/'
         }
